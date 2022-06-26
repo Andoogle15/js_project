52fbc79080c22420a45d132f482f45b8ae1964b4
Andoogle15 <penzov.r.i.20@gmail.com>
2022-06-26 18:41:55 +0300

    Пункты 12.4-12.5

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,47 @@
 var express = require('express')
 var router = express.Router()
 var Hero = require("../models/hero").Hero
-
+var User = require("./../models/User").User
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
-    req.session.greeting = "Hi!!!!"
-    res.render('index', {
-                            title: 'Loony Tunes Show',
-                            menu: menu, counter:req.session.counter 
-                        });
-    })
-});
+    Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
+      req.session.greeting = "Hi!!!!"
+      res.render('index', {
+                              title: 'Loony Tunes Shows',
+                              menu: menu,
+                              counter: req.session.counter
+                          });
+      })
+  });
 
 /* GET login/registration page. */
 router.get('/logreg', function(req, res, next) {
-  res.render('logreg',{title: 'Вход'});
-  });
+  res.render('logreg',{title: 'Вход', error: null});
+});
   
-
+/* POST login/registration page. */
+router.post('/logreg', function(req, res, next) {
+  var username = req.body.username
+  var password = req.body.password
+  User.findOne({username:username},function(err,user){
+    if(err) return next(err)
+    if(user){
+      if(user.checkPassword(password)){
+        req.session.user = user
+        res.redirect('/')
+      } else {
+        res.render('logreg', {title: 'Вход', error: 'Пароль не верный'})
+      }
+    } else {
+      var user = new User({username:username,password:password})
+            user.save(function(err,user){
+                if(err) return next(err)
+                req.session.user = user
+                res.redirect('/')
+            })     
+    }
+})
+});
 
 module.exports = router;
